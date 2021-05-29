f7886462934e9eb2515472227e8797d797454ca1
reynardchu <84781352+reynardchu@users.noreply.github.com>
2021-05-29 13:01:22 +0800

    Update myscript.js

--- webusb/myscript.js
+++ webusb/myscript.js
@@ -4,8 +4,27 @@ document.addEventListener('DOMContentLoaded', event => {
   button.addEventListener('click', async() => {
     let device
     /* const VENDOR_ID = 0x1A61 */
-    const VENDOR_ID = 0x0BB4
-    
+const VENDOR_ID = 0x0BB4
+
+usbDevice.controlTransferIn(({
+		 requestType: 'vendor',
+   		 recipient: 'device',
+   		 request: 51, // ACCESSORY_GET_PROTOCOL
+   		 value: 0,
+   		 index: 0
+}), 2).then(response => { 
+      if (response.data.getInt8(0) == 1) {
+        console.log("AoA V1 available");
+      }
+      if (response.data.getInt8(0) == 2) {
+	console.log("AoA V2 available");
+      }
+  }).catch(e => {
+   	console.log("Can't send " + e);
+});
+	  
+	  
+	  
     try {
       device = await navigator.usb.requestDevice({
         filters: [{
@@ -22,3 +41,15 @@ document.addEventListener('DOMContentLoaded', event => {
 	  await device.close()
   })
 })
+
+
+/*document.querySelector("#request-any").addEventListener('click', function(event) {
+   	 navigator.usb.requestDevice({filters: []})
+	   .then(usbDevice => {
+	      console.log("Product name: " + usbDevice.productName);
+		   // usbDevice 
+	 }) .catch(e => {
+		  console.log(e);
+	 });
+ });
+*/
